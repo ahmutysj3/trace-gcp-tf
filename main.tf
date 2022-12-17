f600eb8be500c2e37f1bfbc52b891f79358b81d5
ahmutysj3 <ahmutysj3@gmail.com>
2022-12-17 11:02:31 -0500

    added s3 backend

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 resource "google_compute_network" "trace" {
-  name = "test-vpc-network-09233483283388"
+  name = "test-vpc-network-new-13-ddjj-22"
 }
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+variable "gcp_project" {
+  type    = string
+  default = "trace-tf-project"
+}
+
+variable "gcp_region" {
+  type    = string
+  default = "us-east1"
+}
